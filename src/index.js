df28d63ecac6ca4ec6b738098de0bcb03c72bab4
niziopawel <niziopawel2@gmail.com>
2021-07-20 09:21:09 +0200

    add reset.css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './components/App'
+import './global-styles/index.css'
+import './global-styles/variables.css'
 
 ReactDOM.render(<App />, document.getElementById('app'))
 
